360ea1f01a7cc9ce341cf51b5acfaf9ef87fe7d2
t-iwano <t-iwano@axsh.net>
2014-03-17 15:06:03 +0900

    test: update wait time for oneTimeSetUp and oneTimeTearDown.

--- test/process/helper_failover.sh
+++ test/process/helper_failover.sh
@@ -15,19 +15,19 @@
 function oneTimeSetUp() {
   status_keepalived ${master} | grep -w running || {
     start_keepalived ${master} 
-    wait_sec 30
+    wait_sec 60
   }
 
   status_keepalived ${backup} | grep -w running || {
     start_keepalived ${backup} 
-    wait_sec 30
+    wait_sec 60
   }
 }
 
 function oneTimeTearDown() {
   status_keepalived ${master} | grep -w "stopped\|locked" && {
     start_keepalived ${master}
-    wait_sec 60
+    wait_sec 120
   }
 }
 
